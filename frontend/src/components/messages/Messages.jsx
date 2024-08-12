f7e293044e354fa49eea76cd494c16d930a5a6d0
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-08-12 11:34:32 +0530

    completed the frontend ui

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,31 @@
+import ReactScrollableFeed from 'react-scrollable-feed';
+import Message from "./Message";
+
+const Messages = () => {
+    return (
+        <ReactScrollableFeed className='px-4 flex-1 overflow-auto'>
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+        </ReactScrollableFeed>
+    );
+};
+
+export default Messages;
