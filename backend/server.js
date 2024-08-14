aaf782670bf25292210edbb1cabd5ead01f8aeda
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-08-14 14:07:59 +0530

    made deployment changes

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,15 @@ import userRoutes from "./routes/user.routes.js";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import path from "path"
+
 
 dotenv.config();
 
 
 const PORT= process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 //Mongo DB connection
 mongoose.connect(process.env.MONGO_DB_URI).then(()=>{
@@ -31,6 +35,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 //api route for the users
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
